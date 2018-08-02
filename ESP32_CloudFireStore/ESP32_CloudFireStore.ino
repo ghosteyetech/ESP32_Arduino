33439d0e1364145c897e7a69208324caace386fa
Sameera Anuradha <sameerat8@gmail.com>
2018-08-02 07:04:11 +0530

    Data saving to firestore is done

--- ESP32_CloudFireStore/ESP32_CloudFireStore.ino
+++ ESP32_CloudFireStore/ESP32_CloudFireStore.ino
@@ -30,10 +30,54 @@ void setup() {
 void loop() {
  
  if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+  getTimeStamp();
+  //saveDataFireStore();
+ }else{
  
-   HTTPClient http;   
+    Serial.println("Error in WiFi connection");   
+ 
+ }
+ 
+  delay(1000);  //Send a request every 1 seconds
+ 
+}
 
-   String url = FIREBASE_DATABSE_URL + "/v1beta1/projects/cloud-firestore-test-62d13/databases/(default)/documents/items?&key=" + FIREBASE_APIKEY;
+void getTimeStamp() {
+  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
+ 
+    HTTPClient http;
+ 
+    http.begin("https://myrestapis-211605.appspot.com/rest/timestamp"); //Specify the URL
+    int httpCode = http.GET();                                        //Make the request
+ 
+    if (httpCode > 0) { //Check for the returning code
+ 
+        String payload = http.getString();
+        http.end(); //Free the resources
+        //Serial.println(httpCode);
+        //Serial.println(payload);
+
+        const size_t bufferSize = JSON_OBJECT_SIZE(1) + 30;
+        DynamicJsonBuffer jsonBuffer(bufferSize);
+        JsonObject& root = jsonBuffer.parseObject(payload);
+        String timestampStr = root["timestamp"];
+        Serial.println(timestampStr);
+        saveDataFireStore(timestampStr);
+      }
+ 
+    else {
+      Serial.println("Error on HTTP request");
+      http.end(); //Free the resources
+    }
+ 
+    
+  }
+}
+
+void saveDataFireStore(String timestampStr){
+  HTTPClient http;   
+
+   String url = FIREBASE_DATABSE_URL + "/v1beta1/projects/cloud-firestore-test-62d13/databases/(default)/documents/temperature?&key=" + FIREBASE_APIKEY;
    http.begin(url);  //Specify destination for HTTP request
    http.addHeader("Content-Type", "application/json");             //Specify content-type header
    
@@ -42,12 +86,18 @@ void loop() {
     JsonObject& JSONencoder = JSONbuffer.createObject(); 
  
     JSONencoder["fields"] = JSONbuffer.createObject();// Declare main object as field (must)
-    JSONencoder["fields"]["aa"] = JSONbuffer.createObject();// define a field want to save(must)
-    JSONencoder["fields"]["aa"]["stringValue"] = "dcdcdc";// define above field data type(must)
+    JSONencoder["fields"]["device"] = JSONbuffer.createObject();// define a field want to save(must)
+    JSONencoder["fields"]["device"]["stringValue"] = "dcdcdc";// define above field data type(must)
+
+    JSONencoder["fields"]["value"] = JSONbuffer.createObject();// define a field want to save(must)
+    JSONencoder["fields"]["value"]["doubleValue"] = random(0, 100);// define above field data type(must)
+
+    JSONencoder["fields"]["timestamp"] = JSONbuffer.createObject();// define a field want to save(must)
+    JSONencoder["fields"]["timestamp"]["doubleValue"] = timestampStr;//timestamp;// define above field data type(must)
  
     char JSONmessageBuffer[300];
     JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-    Serial.println(JSONmessageBuffer);
+    //Serial.println(JSONmessageBuffer);
     
     int httpResponseCode = http.POST(JSONmessageBuffer);   //Send the actual POST request
 
@@ -68,15 +118,6 @@ void loop() {
  
    }
  
-   http.end();  //Free resources
- 
- }else{
- 
-    Serial.println("Error in WiFi connection");   
- 
- }
- 
-  delay(1000);  //Send a request every 1 seconds
- 
+   http.end();  //Free resources  
 }
 
